e68246e27defea9d5630a60693cd818c027a2217
NelliShevchenko <gho020507@gmail.com>
2024-11-01 15:57:31 +0200

    header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function () {
-    $('.bike-slider').slick();  
+    $('.bike-slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });  
 });
